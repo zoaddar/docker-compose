f8039d3d00ca20eec3698ab1018514571c245494
Abdul Kader <hm.qader@gmail.com>
2020-03-07 14:33:10 +0600

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,14 @@
-mysql -uroot -pKader_136_420 bteb_result < bteb_result.sql
-mysql -uroot -pKader_136_420 edusmart < edusmart.sql
-mysql -uroot -pKader_136_420 nixtec_host < nixtec_host.sql
-mysql -uroot -pKader_136_420 zoaddar_emp_att < zoaddar_emp_att.sql
-mysql -uroot -pKader_136_420 zoaddar_sarkarpress < zoaddar_sarkarpress.sql
-mysql -uroot -pKader_136_420 zoaddar_sms_gateway < zoaddar_sms_gateway.sql
+<h1>Hello Cloudreach!</h1>
+<h4>Attempting MySQL connection from php...</h4>
+<?php
+$host = 'mysql';
+$user = 'root';
+$pass = 'Kader_136_420';
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL successfully!";
+}
+?>

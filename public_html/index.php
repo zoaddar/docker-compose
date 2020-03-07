3e470fd90f6da2ddf7185b78df9893f8e00084cd
zoaddar <hm.qader@gmail.com>
2020-03-07 14:30:11 +0600

    PHP/Apache/MySQL/phpMyAdmin

---
+++ public_html/index.php
@@ -0,0 +1,6 @@
+mysql -uroot -pKader_136_420 bteb_result < bteb_result.sql
+mysql -uroot -pKader_136_420 edusmart < edusmart.sql
+mysql -uroot -pKader_136_420 nixtec_host < nixtec_host.sql
+mysql -uroot -pKader_136_420 zoaddar_emp_att < zoaddar_emp_att.sql
+mysql -uroot -pKader_136_420 zoaddar_sarkarpress < zoaddar_sarkarpress.sql
+mysql -uroot -pKader_136_420 zoaddar_sms_gateway < zoaddar_sms_gateway.sql
